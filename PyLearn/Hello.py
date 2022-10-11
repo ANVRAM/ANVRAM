c7bfac42c975b1ab0c175488b141862b8b970c78
ANVRAM <50441931+ANVRAM@users.noreply.github.com>
2022-10-11 20:25:49 +0530

    Update Hello.py

--- PyLearn/Hello.py
+++ PyLearn/Hello.py
@@ -6,7 +6,8 @@ from turtle import fillcolor
 
 Msg="Hello WORLD!!!! form local"
 Msg1="U have a family names as PPAP ALSO PACN AND RJH and one on the way "
-
-print (Msg,Msg1.format)
+Msg2=" Adding Big Bro Law   PAcn family" 
+Msg3=" Adding  Small Bro Law Family RJh and one on the way "
+print (Msg,Msg1.format,Msg2.format,Msg3.format)
 #Msg=Msg.center(20,fillchar='%')
 print (Msg.center(50,filelist))
