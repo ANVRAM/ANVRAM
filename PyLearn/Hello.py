2668bc6c59e74a2e40c3b23a079b36c2cd9eed85
anvram <maruthi.akula7@gmail.com>
2022-10-11 21:12:44 +0530

    Py learn files modified  in local TEST-BRANCH

--- PyLearn/Hello.py
+++ PyLearn/Hello.py
@@ -1,4 +1,4 @@
-#import pandas as pd <its not working now as pandas were not available in system>
+#import pandas as pd #<its not working now as pandas were not available in system>
 
 from distutils import filelist
 from turtle import fillcolor
@@ -8,6 +8,6 @@ Msg="Hello WORLD!!!! form local"
 Msg1="U have a family names as PPAP ALSO PACN AND RJH and one on the way "
 Msg2=" Adding Big Bro Law   PAcn family" 
 Msg3=" Adding  Small Bro Law Family RJh and one on the way "
-print (Msg,Msg1.format,Msg2.format,Msg3.format)
+#print (Msg,Msg1.format,Msg2.format,Msg3.format)
 #Msg=Msg.center(20,fillchar='%')
 print (Msg.center(50,filelist))
