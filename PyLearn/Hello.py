695d69add1ab0e71adff254f8cd9c7c7d4e2701e
anvram <maruthi.akula7@gmail.com>
2022-10-11 20:39:17 +0530

    Py learn files modified  in local TEST-BRANCH-CLI

--- PyLearn/Hello.py
+++ PyLearn/Hello.py
@@ -6,7 +6,8 @@ from turtle import fillcolor
 
 Msg="Hello WORLD!!!! form local"
 Msg1="U have a family names as PPAP ALSO PACN AND RJH and one on the way "
+Msg3="created in branch TEST-BRANCH-CLI "
 
-print (Msg,Msg1.format)
+print (Msg,Msg1.format,Msg3.format)
 #Msg=Msg.center(20,fillchar='%')
 print (Msg.center(50,filelist))
